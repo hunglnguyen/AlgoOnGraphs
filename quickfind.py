9cc0d085286ff33e98884c446f0f1a4a0ea10647
hunglnguyen <nguyenluonghung@hotmail.com>
2021-10-17 09:55:54 -0700

    Started week 1

---
+++ quickfind.py
@@ -0,0 +1,23 @@
+class UnionFind:
+
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+
+    def find(self, x):
+        return self.root[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            for i in range(len(self.root)):
+                if self.root[i] == rootY:
+                    self.root[i] == rootX
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+
+if __name__ == '__main__':
+    union = UnionFind(10)
+    print(union.root)
